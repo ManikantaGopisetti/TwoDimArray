6c214b583215d9b33207d7d07d9ee93411fc4afa
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-08 15:59:24 +0530

    added double and boolean array methods

--- src/core/functionalprograms/TwoDimArray.java
+++ src/core/functionalprograms/TwoDimArray.java
@@ -3,31 +3,108 @@ package core.functionalprograms;
 import java.util.Scanner;
 
 public class TwoDimArray {
-	public static void main(String[] args) {
-		
-		
-		Scanner scan=new Scanner(System.in);
-		System.out.print("Enter n value : ");
-		int n = scan.nextInt();
-		System.out.print("Enter m value : ");
-		int m= scan.nextInt();
-		
-		int a[][]=new int[n][m];
-		
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				System.out.print("Enter array value at index ("+i+","+j+") : ");
-				a[i][j]=scan.nextInt();
+
+	final static int integerarr = 0;
+	final static int doublearr = 1;
+	final static int booleanarr = 2;
+
+	public int[][] intarr(int m, int n) {
+		Scanner scan = new Scanner(System.in);
+		int a[][] = new int[m][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print("Enter array value at index (" + i + "," + j + ") : ");
+				a[i][j] = scan.nextInt();
 			}
-			
+
 		}
 		System.out.print("Given 2D array values are : ");
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				System.out.print(a[i][j]+" ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(a[i][j] + " ");
 			}
 		}
-		
+
+		return a;
+
+	}
+
+	public double[][] darr(int m, int n) {
+		Scanner scan = new Scanner(System.in);
+
+		double a[][] = new double[m][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print("Enter array value at index (" + i + "," + j + ") : ");
+				a[i][j] = scan.nextDouble();
+			}
+
+		}
+		System.out.print("Given 2D array values are : ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+		}
+
+		return a;
+
+	}
+
+	public boolean[][] barr(int m, int n) {
+		Scanner scan = new Scanner(System.in);
+		boolean a[][] = new boolean[m][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print("Enter array value at index (" + i + "," + j + ") : ");
+				a[i][j] = scan.nextBoolean();
+			}
+
+		}
+		System.out.print("Given 2D array values are : ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+		}
+
+		return a;
+	}
+
+	public static void main(String[] args) {
+
+		TwoDimArray ta = new TwoDimArray();
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter row size : ");
+		int n = scan.nextInt();
+		System.out.print("Enter column size : ");
+		int m = scan.nextInt();
+
+		System.out.println("choose 0 for integer array ");
+		System.out.println("choose 1 for double array ");
+		System.out.println("choose 2 for boolean array ");
+
+		int option = scan.nextInt();
+
+		switch (option) {
+		case integerarr:
+			int intarr[][] = ta.intarr(m, n);
+			break;
+		case doublearr:
+			double darr[][] = ta.darr(m, n);
+			break;
+		case booleanarr:
+			boolean barr[][] = ta.barr(m, n);
+			break;
+		default:
+			System.out.println("choose a valid option");
+			break;
+		}
+
 	}
 
 }
